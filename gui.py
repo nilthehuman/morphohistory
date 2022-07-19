f5fb1ef02bb7218e78c1fc5393efca55946403c3
Dániel Arató <nil.the.human@gmail.com>
2022-07-19 15:37:38 +0200

    Exit on cue

--- gui.py
+++ gui.py
@@ -10,6 +10,23 @@ from kivy.uix.behaviors import DragBehavior
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
+# Adapted from kivy.org/doc/stable/api-kivy.core.window.html
+class KeyeventHandler(Widget):
+    def __init__(self, **kwargs):
+        super(KeyeventHandler, self).__init__(**kwargs)
+        self.keyboard = Window.request_keyboard(self.on_keyboard_closed, self)
+        self.keyboard.bind(on_key_down=self.on_keypressed)
+
+    def on_keyboard_closed(self):
+        self.keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self.keyboard = None
+
+    def on_keypressed(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'q':
+            App.get_running_app().stop()
+            return True
+        return False
+
 class Disc(DragBehavior, Widget):
     def __init__(self, **kwargs):
         self.color = 0.6, 0.4, 0.8
@@ -17,8 +34,12 @@ class Disc(DragBehavior, Widget):
         self.size = 20, 20
         self.pos = Window.width * 0.5, Window.height * 0.5
 
+    def on_pos(self, *args):
+        pass #print(self.x, self.y)
+
 class MurmurApp(App):
     def build(self):
+        KeyeventHandler()
         return Disc()
 
 if __name__ == '__main__':
