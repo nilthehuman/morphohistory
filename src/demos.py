0ee4bb8188dfa7c94451e8d0913c1c2384ab834b
Dániel Arató <nil.the.human@gmail.com>
2023-02-09 17:59:08 +0100

    Substitute size parameters in demos

--- src/demos.py
+++ src/demos.py
@@ -3,7 +3,11 @@
 from abc import ABC
 from math import sin, cos, pi
 
+from .settings import SETTINGS
 from .speaker import Speaker
+
+WIDTH = 600
+HEIGHT = 600
 
 class DemoFactory(ABC):
     """Interface for demo generating factory classes."""
@@ -19,7 +23,8 @@ class Rainbow9x9(DemoFactory):
         for row in range(9):
             for col in range(9):
                 weight_a = 1 - 1 / 16 * (row + col)
-                pos = self.width * 0.1 + self.width * 0.1 * col - 10, self.height * 0.9 - self.height * 0.1 * row - 10
+                pos[0] = WIDTH * 0.1 + WIDTH * 0.1 * col - SETTINGS.speakerdot_size[0] * 0.5
+                pos[1] = HEIGHT * 0.9 - HEIGHT * 0.1 * row - SETTINGS.speakerdot_size[1] * 0.5
                 speakers.append(Speaker.fromweight(row * 10 + col, pos, weight_a))
         return speakers
 
@@ -31,7 +36,8 @@ class Rainbow10x10(DemoFactory):
         for row in range(10):
             for col in range(10):
                 weight_a = 1 - 1 / 18 * (row + col)
-                pos = self.width * 0.05 + self.width * 0.1 * col - 10, self.height * 0.95 - self.height * 0.1 * row - 10
+                pos[0] = WIDTH * 0.05 + WIDTH * 0.1 * col - SETTINGS.speakerdot_size[0] * 0.5
+                pos[1] = HEIGHT * 0.95 - HEIGHT * 0.1 * row - SETTINGS.speakerdot_size[1] * 0.5
                 speakers.append(Speaker.fromweight(row * 10 + col, pos, weight_a))
         return speakers
 
@@ -42,7 +48,8 @@ class Synonymy(DemoFactory):
         speakers = []
         for row in range(10):
             for col in range(10):
-                pos = self.width * 0.05 + self.width * 0.1 * col - 10, self.height * 0.95 - self.height * 0.1 * row - 10
+                pos[0] = WIDTH * 0.05 + WIDTH * 0.1 * col - SETTINGS.speakerdot_size[0] * 0.5
+                pos[1] = HEIGHT * 0.95 - HEIGHT * 0.1 * row - SETTINGS.speakerdot_size[1] * 0.5
                 speakers.append(Speaker.fromweight(row * 10 + col, pos, 0.5))
         return speakers
 
@@ -53,7 +60,8 @@ class CoreVsPeriphery10x10(DemoFactory):
         speakers = []
         for row in range(10):
             for col in range(10):
-                pos = self.width * 0.05 + self.width * 0.1 * col - 10, self.height * 0.95 - self.height * 0.1 * row - 10
+                pos[0] = WIDTH * 0.05 + WIDTH * 0.1 * col - SETTINGS.speakerdot_size[0] * 0.5
+                pos[1] = HEIGHT * 0.95 - HEIGHT * 0.1 * row - SETTINGS.speakerdot_size[1] * 0.5
                 if (3 <= row and row <= 6 and 3 <= col and col <= 6):
                     speakers.append(Speaker.fromweight(row * 10 + col, pos, 0.0))
                 else:
@@ -66,11 +74,14 @@ class NewsAnchor(DemoFactory):
     def get_speakers():
         speakers = []
         for n in range(16):
-            x = sin(2 * pi * float(n) / 16) * 180
-            y = cos(2 * pi * float(n) / 16) * 180
-            pos = (300 + x - 10, 300 + y - 10)
+            x = sin(2 * pi * float(n) / 16) * WIDTH * 0.3
+            y = cos(2 * pi * float(n) / 16) * HEIGHT * 0.3
+            pos[0] = WIDTH * 0.5 + x - SETTINGS.speakerdot_size[0] * 0.5
+            pos[1] = HEIGHT * 0.5 + y - SETTINGS.speakerdot_size[1] * 0.5
             speakers.append(Speaker.fromweight(n, pos, 0.5))
-        broadcaster = speakers.append(Speaker.fromweight(16, (300 - 10, 300 - 10), 1.0, is_broadcaster=True))
+        pos[0] = WIDTH * 0.5 - SETTINGS.speakerdot_size[0] * 0.5
+        pos[1] = HEIGHT * 0.5 - SETTINGS.speakerdot_size[1] * 0.5
+        broadcaster = speakers.append(Speaker.fromweight(16, pos, 1.0, is_broadcaster=True))
         return speakers
 
 class Rings16_24(DemoFactory):
@@ -79,13 +90,15 @@ class Rings16_24(DemoFactory):
     def get_speakers():
         speakers = []
         for n in range(16):
-            x = sin(2 * pi * float(n) / 16) * 120
-            y = cos(2 * pi * float(n) / 16) * 120
-            pos = (300 + x - 10, 300 + y - 10)
+            x = sin(2 * pi * float(n) / 16) * WIDTH * 0.2
+            y = cos(2 * pi * float(n) / 16) * HEIGHT * 0.2
+            pos[0] = WIDTH * 0.5 + x - SETTINGS.speakerdot_size[0] * 0.5
+            pos[1] = HEIGHT * 0.5 + y - SETTINGS.speakerdot_size[1] * 0.5
             speakers.append(Speaker.fromweight(n, pos, 0.0))
         for n in range(24):
-            x = sin(2 * pi * float(n) / 24) * 240
-            y = cos(2 * pi * float(n) / 24) * 240
-            pos = (300 + x - 10, 300 + y - 10)
+            x = sin(2 * pi * float(n) / 24) * WIDTH * 0.4
+            y = cos(2 * pi * float(n) / 24) * HEIGHT * 0.4
+            pos[0] = WIDTH * 0.5 + x - SETTINGS.speakerdot_size[0] * 0.5
+            pos[1] = HEIGHT * 0.5 + y - SETTINGS.speakerdot_size[1] * 0.5
             speakers.append(Speaker.fromweight(16 + n, pos, 0.0))
         return speakers
