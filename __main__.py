cd1291b56b19770bda0358ef1cb9ffb3ec12433b
Dániel Arató <nil.the.human@gmail.com>
2023-02-10 19:28:50 +0100

    Issue warning if Python version is too old

--- __main__.py
+++ __main__.py
@@ -1,3 +1,9 @@
+from sys import version_info
+from kivy.logger import Logger
+
+if version_info < (3, 7):
+    Logger.warning("You're using an old version of Python. Please consider upgrading to Python 3.7 or newer.")
+
 from src.gui import MurmurApp
 
 MurmurApp().run()
