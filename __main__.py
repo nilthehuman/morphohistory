eeb6b3c501a87b32c3c3d6ea387fa5772f5e9dfb
Dániel Arató <nil.the.human@gmail.com>
2023-02-10 22:26:13 +0100

    Try both relative and absolute import when starting app

--- __main__.py
+++ __main__.py
@@ -4,6 +4,9 @@ from kivy.logger import Logger
 if version_info < (3, 7):
     Logger.warning("You're using an old version of Python. Please consider upgrading to Python 3.7 or newer.")
 
-from src.gui.app import MurmurApp
+try:
+    from .src.gui.app import MurmurApp
+except ImportError:
+    from src.gui.app import MurmurApp
 
 MurmurApp().run()
