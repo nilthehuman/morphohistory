c01df330ea86382a0d6569731d604f5a9d325b60
Dániel Arató <nil.the.human@gmail.com>
2023-02-12 16:03:15 +0100

    Use the MD5 hash of my second nieces bashing the keyboard as random seed

--- src/rng.py
+++ src/rng.py
@@ -6,8 +6,8 @@ try:
     from simplerandom.iterators import MWC1
     from bisect import bisect
     class _RNG:
-        def __init__(self):
-            self.mwc = MWC1() # unseeded is fine
+        def __init__(self, random_seed):
+            self.mwc = MWC1(random_seed)
         def next(self):
             return next(self.mwc)
         def choices(self, population, cum_weights):
@@ -17,11 +17,15 @@ try:
             # copied this trick from the original Lib/random.py
             return [population[bisect(cum_weights, r * scale, 0, len(population) - 1)]]
 except ImportError:
-    from random import choices, randrange
+    from random import choices, randrange, seed
     class _RNG:
+        def __init__(self, random_seed):
+            seed(random_seed)
         def next(self):
             return randrange(0, 2**32)
         def choices(self, population, cum_weights):
             return choices(population, cum_weights=cum_weights)
 
-RAND = _RNG()
+# random seed courtesy of Lia & Bella
+_LIA_BELLA_MD5 = 0xae10de48e999b5ba913ac8cd5cc32155
+RAND = _RNG(_LIA_BELLA_MD5)
