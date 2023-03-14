b406f308db3f5247efc49cf9c7ec60615b898207
Dániel Arató <nil.the.human@gmail.com>
2023-03-14 12:09:49 +0100

    Retire unnecessary relative import attempt

--- __main__.py
+++ __main__.py
@@ -4,9 +4,6 @@ from logging import warning
 if version_info < (3, 7):
     warning("You're using an old version of Python. Please consider upgrading to Python 3.7 or newer.")
 
-try:
-    from .src.gui.app import MorphoHistoryApp
-except ImportError:
-    from src.gui.app import MorphoHistoryApp
+from src.gui.app import MorphoHistoryApp
 
 MorphoHistoryApp().run()
