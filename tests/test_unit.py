9f3266cbb1087eeb22e74f50778a8be6ab6df24b
Dániel Arató <nil.the.human@gmail.com>
2023-03-15 09:56:55 +0100

    Unit tests for l10n

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,8 @@
 """Unit tests to check basic expected behaviors."""
 
+from ..src.gui.l10n import localize, unlocalize
 from ..src.paradigm import *
+from ..src.settings import SETTINGS
 
 def test_always_pass():
     assert True
@@ -16,3 +18,21 @@ def test_NounParadigm_assignment():
     noun_para = NounParadigm()
     noun_para[0][0].bias_a = 0.12345
     assert noun_para[0][0].bias_a == 0.12345
+
+def test_l10n_to_English():
+    SETTINGS.gui_language = SETTINGS.GuiLanguage.ENG
+    assert "Start" == localize("Start")
+    assert "%d iterations" % 13 == localize("%d iterations") % 13
+    assert "abracadabra" == localize("abracadabra")
+    assert "Start" == unlocalize("Start")
+    assert "%d iterations" % 13 == unlocalize("%d iterations") % 13
+    assert "abracadabra" == unlocalize("abracadabra")
+
+def test_l10n_to_Hungarian():
+    SETTINGS.gui_language = SETTINGS.GuiLanguage.HUN
+    assert "Csapassad neki!" == localize("Start")
+    assert "%d iteráció" % 13 == localize("%d iterations") % 13
+    assert "abracadabra" == localize("abracadabra")
+    assert "Start" == unlocalize(localize("Start"))
+    assert "%d iterations" % 13 == unlocalize(localize("%d iterations")) % 13
+    assert "abracadabra" == unlocalize(localize("abracadabra"))
