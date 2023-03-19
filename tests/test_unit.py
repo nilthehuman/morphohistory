00c51516b1bdd7c9140cb5ad15afd3186c4e8d24
Dániel Arató <nil.the.human@gmail.com>
2023-03-19 18:40:00 +0100

    Very basic unit tests for a single simulation step

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,7 +1,9 @@
 """Unit tests to check basic expected behaviors."""
 
 from ..src.gui.l10n import localize, unlocalize
-from ..src.paradigm import *
+from ..src.agora import Agora
+from ..src.paradigm import CellIndex, NounParadigm
+from ..src.agora import Speaker
 from ..src.settings import SETTINGS
 
 def test_always_pass():
@@ -10,14 +12,47 @@ def test_always_pass():
 def test_default_NounParadigm():
     noun_para = NounParadigm()
     assert noun_para[0][0].bias_a == 0.5
+    assert noun_para[CellIndex(0,0)].bias_a == 0.5
     assert noun_para[0][0].form_a == ''
+    assert noun_para[CellIndex(0,0)].form_a == ''
     assert noun_para[0][0].form_b == ''
+    assert noun_para[CellIndex(0,0)].form_b == ''
     assert noun_para[0][0].prominence == 1.0
+    assert noun_para[CellIndex(0,0)].prominence == 1.0
 
 def test_NounParadigm_assignment():
     noun_para = NounParadigm()
     noun_para[0][0].bias_a = 0.12345
     assert noun_para[0][0].bias_a == 0.12345
+    assert noun_para[CellIndex(0,0)].bias_a == 0.12345
+
+def test_simulation_harmonic():
+    SETTINGS.sim_learning_model = SETTINGS.LearningModel.HARMONIC
+    agora = Agora()
+    noun_para_tomayto = NounParadigm(0.0, 'tomahto', 'tomayto')
+    agora.add_speaker(Speaker(0, (-100, 0), noun_para_tomayto))
+    noun_para_tomahto = NounParadigm(1.0, 'tomahto', 'tomayto')
+    agora.add_speaker(Speaker(1, (+100, 0), noun_para_tomahto))
+    assert 0.0 == agora.state.speakers[0].principal_bias()
+    assert 1.0 == agora.state.speakers[1].principal_bias()
+    agora.simulate()
+    assert (0.0 == agora.state.speakers[0].principal_bias() and 0.5 == agora.state.speakers[1].principal_bias()) or \
+           (0.5 == agora.state.speakers[0].principal_bias() and 1.0 == agora.state.speakers[1].principal_bias())
+    assert 2 == agora.state.speakers[0].experience and 2 == agora.state.speakers[1].experience
+
+def test_simulation_RW_vanilla():
+    SETTINGS.sim_learning_model = SETTINGS.LearningModel.RW
+    agora = Agora()
+    noun_para_tomayto = NounParadigm(0.0, 'tomahto', 'tomayto')
+    agora.add_speaker(Speaker(0, (-100, 0), noun_para_tomayto))
+    noun_para_tomahto = NounParadigm(1.0, 'tomahto', 'tomayto')
+    agora.add_speaker(Speaker(1, (+100, 0), noun_para_tomahto))
+    assert 0.0 == agora.state.speakers[0].principal_bias()
+    assert 1.0 == agora.state.speakers[1].principal_bias()
+    agora.simulate()
+    assert (0.0 == agora.state.speakers[0].principal_bias() == agora.state.speakers[1].principal_bias()) or \
+           (1.0 == agora.state.speakers[0].principal_bias() == agora.state.speakers[1].principal_bias())
+    assert 2 == agora.state.speakers[0].experience and 2 == agora.state.speakers[1].experience
 
 def test_l10n_to_English():
     SETTINGS.gui_language = SETTINGS.GuiLanguage.ENG
